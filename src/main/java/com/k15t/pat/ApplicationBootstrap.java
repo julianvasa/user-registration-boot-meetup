4c420784fa85f2757655d4c1c8e8576fa030a881
julianvasa <vasa.julian@gmail.com>
2019-11-12 22:35:56 +0100

    Remove old configurations

--- src/main/java/com/k15t/pat/ApplicationBootstrap.java
+++ src/main/java/com/k15t/pat/ApplicationBootstrap.java
@@ -1,20 +1,10 @@
 package com.k15t.pat;
 
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class ApplicationBootstrap {
 
     public static void main(String[] args) {
@@ -22,21 +12,5 @@ public class ApplicationBootstrap {
     }
 
 
-    @Bean
-    public ServletRegistrationBean initJerseyServlet() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/rest/*");
-        registration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName());
-        return registration;
-    }
-
-    @Bean
-    public WebMvcConfigurerAdapter forwardToIndex() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("forward:/registration.html");
-            }
-        };
-    }
 
 }
